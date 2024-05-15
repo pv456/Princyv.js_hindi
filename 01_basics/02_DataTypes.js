9aa39abe53ea3a9f463ae01c1dec2f8ab333c4a2
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-15 17:27:30 +0000

    const config done

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log(3
+    +
+    3)// code readablitity should be high
+
+console.log("Princy")
+
+
+let name = "Princy"
+let age = 16
+let isloggIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
