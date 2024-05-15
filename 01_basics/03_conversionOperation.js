1c4d971f931e46f32a3daf82a920b0c6ebc8dfad
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-15 18:50:00 +0000

    Const config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "Princy"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber );
+//console.log( valueInNumber );
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isloggedIn = 1 
+
+let boolenIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
