16f4381859c2552ff6dd7d2395f46a0c3f7dd4f8
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-15 17:02:43 +0000

    Const config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+Const accountId = 6685541
+let accountEmail = "Princy@gmail.com"
+var accountPassword = "8854911"
+accountCity = "Aliganj"
+
+// accountId =2 //not allowed
+
+accountEmail = Pc@pc.com"
+accountPassword = "88744667"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+
