519111233db581e2cf0a16caefb5952484625b23
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-15 17:06:00 +0000

    const config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,11 +5,17 @@ accountCity = "Aliganj"
 
 // accountId =2 //not allowed
 
+
 accountEmail = Pc@pc.com"
 accountPassword = "88744667"
 accountCity = "Mumbai"
 
 console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
 
