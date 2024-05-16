ac4afbb5d7b27d54c945833300a6af2199d7dfe4
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-16 15:47:24 +0000

    Const config done

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// # Primitive
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 6532388977424674454n
+
+
+
+// Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktima", "naagraj", "doga"]
+let myobj ={
+    name: "princy",
+    age: 13,
+}
+
+const myfunction =function(){
+    console.log("Hello world");
+}
+
+console.log(typeof accountId;
