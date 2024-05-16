d74794afea22c62c8e0ae042638502c451f363bd
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-16 14:04:00 +0000

    const config done

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = 33
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; False => 0
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Princy" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
