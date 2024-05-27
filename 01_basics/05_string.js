cd12f0b183a27e6a7a435cd866261fadecc759e6
pv456 <168293268+pv456@users.noreply.github.com>
2024-05-27 10:26:09 +0000

    const config done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "Princy"
+const repoCount = 15
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello mt name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Princy')
