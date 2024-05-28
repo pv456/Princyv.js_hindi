e208d959a03ff20b2618ddc4b68ddf0eb2b816d0
Princy Verma <168293268+pv456@users.noreply.github.com>
2024-05-28 10:51:05 +0000

    const config done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -18,3 +18,7 @@ console.log(gameName.indexOf('c'));
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
