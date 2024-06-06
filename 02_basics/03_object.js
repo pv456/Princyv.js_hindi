266cc207c2068a5f2212cb07f89e8fbc3f617567
Princy Verma <168293268+pv456@users.noreply.github.com>
2024-06-06 10:16:13 +0000

    const config done

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -2,9 +2,14 @@
 // object.create
 
 // object literals
+
+const mySym = Symbol("key1")
+
+
 const JsUser = {
     name: "Princy",
-    "Full name": "Princy Verma",
+    "full name": "Princy Verma",
+    [mySym]: "mykey1",
     age: 15,
     location: "Mumbai",
     email: "Princy@google.com",
@@ -12,6 +17,19 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"],
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["Full name"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "Princy@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "Princy@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greetingTwo = function() {
+    console.log('Hello Js user, ${this.name}');
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
