3440d7badcf690f967fe617f377dbd3256630fdb
Princy Verma <168293268+pv456@users.noreply.github.com>
2024-06-14 16:14:56 +0000

    const done

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -46,10 +46,31 @@ const user = [
 ]
 
 user[1]. email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('islogged'));
+// console.log(tinderUser.hasOwnProperty('islogged'));
+
+
+const course = {
+    course:  "js in hindi",
+    price: "999",
+    courseInstructor: "Princy"
+}
+
+// course.courseInstructor 
+
+const {courseInstructor:instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+const navbar = (props.comapany) => {
+
+
+}
+navbar(campany = "Princy")
