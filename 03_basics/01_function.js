4deeb4dc3449edaf0f18ab79f2532d430db671f7
Princy Verma <168293268+pv456@users.noreply.github.com>
2024-06-17 12:01:42 +0000

    taking parameters in functions

---
+++ 03_basics/01_function.js
@@ -0,0 +1,25 @@
+
+function sayMyName(){
+    console.log("P");
+    console.log("R");
+    console.log("I");
+    console.log("N");
+    console.log("C");
+    console.log("Y"); 
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1 , number2){
+    
+//              console.log( number1 + number2);
+// }
+function addTwoNumbers(number1 , number2){
+    
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(4,4)
+
+console.log("Result:",result);
